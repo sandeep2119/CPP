8a7863083788e8e47c277050b33f77f11481b19e
sandeep2119 <54413323+sandeep2119@users.noreply.github.com>
2019-08-30 01:23:49 -0700

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,37 @@
+// Write a program that takes an input of string and displays the reverse of the string. 
+//It should be a function that takes a char*  as arg and returns the char* with reversed value.
+
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+int n;
+void StringRev(string *str,string *revstr){
+	cout<<"Enter the size of the string"<<endl;
+	cin>>n;
+	string *str[n];
+	cout<<"Enter The String : "<<endl;
+	cin >> *str;
+	cout<<"The Reverse String is : "<<string *_strrev(string *_str);
+	
+}
+
+
+//int len(){
+//	
+//	char array[] = "Sandeep";  
+//    cout << "Reverse of String = " << strrev(array)<<endl;  
+//    return 0; 
+//}
+
+
+
+int main() {
+	
+//	len();
+	string *str = "Sandeep";
+	string *revstr;
+	
+	StringRev(*str,*revstr);
+	return 0;
+}
